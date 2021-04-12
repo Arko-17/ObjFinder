b1eefd1bcf7b91b5ef03e083bdb7ce660eba5f43
Arko-17 <74445208+Arko-17@users.noreply.github.com>
2021-04-12 21:48:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 status="";
 video="";
+object=[];
 function draw(){
-image(video,0,0,480,380);
-}
+    image(video,0,0,480,380);
+    if(status !=""){
+        objectDetector.detect(video,gotResult);
+        for(i=0;i<object.length;i++){
+            document.getElementById("status").innerHTML="Status: Object has been detected";
+            document.getElementById("objectcount").innerHTML="No. of objects detected are: "+object.length;
+    
+            percent=floor(object[i].confidence*100);
+            text(object[i].label+" "+percent+" %",object[i].x+15,object[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height,);
+        }
+    }
+    }
 function setup(){
 canvas=createCanvas(480,380);
 canvas.center();
@@ -18,4 +32,13 @@ document.getElementById("objname").value;
 function modelLoaded(){
 console.log("The model has been loaded thank you for your patience (:");
 status=true;
+}
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object=results;
+    }
 }
